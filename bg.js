6eafab8c28f5b03609f1ca5ecb460d4674a7e585
tsglow <haruki84@msn.com>
2020-10-18 22:14:06 +0900

    sa

--- bg.js
+++ bg.js
@@ -1,15 +1,9 @@
-const body = document.querySelector("body");
-
 const IMG_NUMBER = 5;
 
 
-
-
 function paintImage(randomNumber){
-    const image = new Image();
-    image.src = `images/${randomNumber + 1 }.jpg`;
-     image.classList.add("bgImage");
-     body.appendChild(image);
+    image = `url(images/${randomNumber + 1 }.jpg)`;
+    document.body.style.backgroundImage = image
 }
 
 function genRandom(){
